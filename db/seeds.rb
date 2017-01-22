5f4690c2742596f7619c1394a1781f74b6912366
Camille <camilleseguette@gmail.com>
2017-01-22 13:10:01 -0800

    Seeding with Faker completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'random_data'
+
+ 50.times do
+   User.create!(
+     name: RandomData.random_sentence
+   )
+ end
+ users = User.all
+
+ 100.times do
+   Item.create!(
+     name: RandomData.random_paragraph
+   )
+ end
+
+ puts "Seed finished"
+ puts "#{User.count} users created"
+ puts "#{Item.count} items created"
